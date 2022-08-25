5dd3e3c35af25198b637d322113503e765e16f2f
Estebanano <esquintero@unal.edu.co>
2022-08-25 09:44:49 -0500

    Funciones f

--- Taller1/Problema1/Epidemia.cpp
+++ Taller1/Problema1/Epidemia.cpp
@@ -1,8 +1,27 @@
 #include "iostream"
 #include "cmath"
 using namespace std;
+
+// Constantes de contagio y tiempo de recuperacion
+const double Beta = 0.35;
+const double Gamma = 0.08;
+
+// Declaracion de funciones
+double f1(double s, double i, double t);
+double f2(double s, double i, double t);
+
 int main(void){
   return 0;
 }
 
+// Implementando funciones
+double f1(double s, double i, double t)
+{
+  return -Beta*s*i;
+}
+
+double f2(double s, double i, double t)
+{
+  return Beta*s*i - Gamma*i;
+}
 
