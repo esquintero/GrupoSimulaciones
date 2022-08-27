4d7083072eff11c79ac459bad6a1d36f19a1f88b
Estebanano <esquintero@unal.edu.co>
2022-08-27 11:45:03 -0500

    Graficas punto c, completado problema 1

--- Taller1/Problema1/graf.py
+++ Taller1/Problema1/graf.py
@@ -1,22 +1,42 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+plt.rcParams["text.usetex"] = True
+
 epidemia = np.loadtxt(
     "c:/Users/Esteban/Desktop/Uni Esteban/Simulaciones/RepositorioGrupoSimulaciones/Taller1/Problema1//datosP1a.txt"
 )
+
 tiempo = epidemia[:, 0]
 susceptibles = epidemia[:, 1]
 infectados = epidemia[:, 2]
 recuperados = epidemia[:, 3]
 
-fig, ax = plt.subplots(figsize=(10, 6))
-ax.plot(tiempo, susceptibles, "b", label="Susceptibles")
-ax.plot(tiempo, infectados, "r", label="Infectados")
-ax.plot(tiempo, recuperados, "g", label="Recuperados")
-ax.set_title("Modelo SIR sin muertes o nacimientos")
-ax.set_xlabel("Tiempo")
-ax.set_ylabel("Susceptibles, Infectados, Recuperados")
-ax.legend(loc=(0.05, 0.7))
-ax.set_xlim(xmin=0, xmax=70)
-ax.set_ylim(ymin=0, ymax=1)
-ax.tight_layout()
+slim = np.loadtxt(
+    "c:/Users/Esteban/Desktop/Uni Esteban/Simulaciones/RepositorioGrupoSimulaciones/Taller1/Problema1/datosP1c.txt"
+)
+
+R0 = slim[:, 0]
+sinf = slim[:, 1]
+
+fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(16, 4))
+
+# Grafica de SIR
+ax1.plot(tiempo, susceptibles, "b", label="Susceptibles")
+ax1.plot(tiempo, infectados, "r", label="Infectados")
+ax1.plot(tiempo, recuperados, "g", label="Recuperados")
+ax1.set_title("Modelo SIR sin muertes o nacimientos")
+ax1.set_xlabel("Tiempo")
+ax1.set_ylabel("Susceptibles, Infectados, Recuperados")
+ax1.legend(loc=(0.0185, 0.7))
+ax1.set_xlim(xmin=0, xmax=70)
+ax1.set_ylim(ymin=0, ymax=1)
+
+# Grafica de S limite
+ax2.plot(R0, sinf, "c", label=r"No. de Susceptibles si t $\rightarrow \infty$")
+ax2.set_title(r"$s_{\infty}$ vs $R_{0}$")
+ax2.legend()
+ax2.set_xlabel(r"R$_{0}=\beta / \gamma$", fontsize=15)
+ax2.set_ylabel(r"s$_{\infty}$", fontsize=15)
+ax2.set_xlim(xmin=R0[0], xmax=R0[-1])
+ax2.set_ylim(ymin=0, ymax=0.014)
