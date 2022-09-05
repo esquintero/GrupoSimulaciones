5986fc44a24db6aa6f8f265a829f09d084103efe
Laura Juliana Caviedes Betancourt <ljcaviedesb@unal.edu.co>
2022-09-04 22:58:34 -0500

    change path file vector.h

--- Taller1/Problema3/Sistema_Sol_Jupiter.cpp
+++ Taller1/Problema3/Sistema_Sol_Jupiter.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cmath>
-#include "../../vector.h"
+#include "../vector.h"
 /////////////////////////////////////////////////////////////////
 // Dos planetas                                                //
 //                                                             //
