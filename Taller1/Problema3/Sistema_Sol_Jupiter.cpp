8770836b8a7ff62b0b90e62de6449721e8007a93
Laura Juliana Caviedes Betancourt <ljcaviedesb@unal.edu.co>
2022-09-06 17:41:39 -0500

    Sistema 2 cuerpos

--- Taller1/Problema3/Sistema_Sol_Jupiter.cpp
+++ Taller1/Problema3/Sistema_Sol_Jupiter.cpp
@@ -14,16 +14,10 @@
 // m_sol = 1047 m_jupiter     ,  r_sol = 9.73 r_jupiter        //
 // m_jupiter = 1              ,  r_jupiter = 1                 //
 //                                                             //
-// Tambien se agrega un tercer objeto al cual se le denomina   //
-// troyano de Júpiter y va a estar ubicado en el punto de      //
-// Lagrange L4 el cuel esta a pi/3 en adelanto a Júpiter. Su   //
-// masa y radio se escogen:                                    //
-// m_troyano = 0.005 m_jupiter ,  r_troyano = 1_jupiter/10     //
 /////////////////////////////////////////////////////////////////
 const double G=1.0;
-// Para el análisis de Sol-Júpiter hacemos N=2. Para
-// Sol-Jupiter-Troyano hacemos N=3
-const int N=3;
+// Para el análisis de Sol-Júpiter hacemos N=2.
+const int N=2;
 
 // Coeficientes de PEFRL
 const double Zeta   = 0.1786178958448091e00;
@@ -96,11 +90,10 @@ void Colisionador::CalculeFuerzaEntre(Cuerpo & Planeta1, Cuerpo &Planeta2){
 int main(){
   Cuerpo Planeta[N];
   Colisionador Newton;
-  double m1=1047, m2=1, m3=0.005, r=1000;
+  double m1=1047, m2=1, r=1000;
   double theta=M_PI/3;
-  double M=m1+m2, x1=-m2*r/M, x2=m1*r/M, x3=x2*cos(theta),y3=x2*sin(theta);
+  double M=m1+m2, x1=-m2*r/M, x2=m1*r/M;
   double omega = sqrt(G*M/pow(r,3.0)), T=2*M_PI/omega, V1=omega*x1, V2=omega*x2;
-  double Vx3=-V2*sin(theta), Vy3=V1*cos(theta);
   double t, dt=0.01;
   double tmax=21.0*T;
   double tdibujo, tcuadro=T/100;
@@ -111,8 +104,6 @@ int main(){
 
   Planeta[0].Inicie(x1, 0,0,  0, V1,0,m1,97.3); // Sol
   Planeta[1].Inicie(x2, 0,0,  0, V2,0,m2,10.0); // Júpiter
-  //  Planeta[2].Inicie(x3,y3,0,Vx3,Vy3,0,m3, 1.0); // Planeta Troyano
-  Planeta[2].Inicie(x3*0.009,y3,0,Vx3,Vy3,0,m3, 1.0); // Planeta Troyano Perturbado
   for(t=0,tdibujo=0; t<tmax; t+=dt, tdibujo+=dt){
     outfile<<Planeta[0].Getx()<<" "<<Planeta[0].Gety()<<" "
 	     <<Planeta[1].Getx()<<" "<<Planeta[1].Gety()<<" "
