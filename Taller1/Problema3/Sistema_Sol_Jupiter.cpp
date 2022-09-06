3e1883fa3aad2eec25408a5941072913e76c704a
Laura Juliana Caviedes Betancourt <ljcaviedesb@unal.edu.co>
2022-09-06 18:00:54 -0500

    add lib

--- Taller1/Problema3/Sistema_Sol_Jupiter.cpp
+++ Taller1/Problema3/Sistema_Sol_Jupiter.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include "../vector.h"
+#include "planetas_lib.h"
 #include <fstream>
 /////////////////////////////////////////////////////////////////
 // Cuerpos Celestes - metodo de integracion PEFRL              //
@@ -26,67 +27,7 @@ const double Chi    = -0.6626458266981849e-1;
 const double Coeficiente1 =(1-2*Lambda)/2;
 const double Coeficiente2 =1-2*(Chi+Zeta);
 
-// Clases
-class Cuerpo;
-class Colisionador;
-//------------------ Clase Cuerpo ------------------------------//
-class Cuerpo{
-private:
-  vector3D r, V, F;
-  double m,R;
-public:
-  void Inicie(double x0,double y0,double z0, double Vx0, double Vy0, double Vz0,double m0,double R0);
-  void BorreFuerza(void);
-  void SumeFuerza(vector3D F0);
-  void Mueva_r(double dt, double coeficiente);
-  void Mueva_v(double dt, double coeficiente);
-  double Getx(void){return r.x();}; //Inline
-  double Gety(void){return r.y();}; //Inline
-  double Getz(void){return r.z();}; //Inline
-  friend class Colisionador; //Cualquier especimen de la clase colisionador le puede meter la mano a la clase cuerpo
-};
-void Cuerpo::Inicie(double x0,double y0,double z0, double Vx0, double Vy0, double Vz0,double m0,double R0){
-  r.load(x0,y0,z0);
-  V.load(Vx0,Vy0,Vz0);
-  m=m0; R=R0;
-}
-void Cuerpo::BorreFuerza(void){
-  F.load(0,0,0);
-}
-void Cuerpo::SumeFuerza(vector3D F0){
-  F+=F0;
-}
-void Cuerpo::Mueva_r(double dt,double coeficiente){
-  vector3D r_new;
-  r+=V*dt*coeficiente;
-} 
-void Cuerpo::Mueva_v(double dt,double coeficiente){
-  vector3D r_new;
-  V+=F*dt*coeficiente/m;
-}
-//------------------ Class Colisionador ------------------------//
-class Colisionador {
-private:
-public:
-  void CalculeFuerza(Cuerpo * Planeta);
-  void CalculeFuerzaEntre(Cuerpo &  Planeta1, Cuerpo & Planeta2);
-};
-void Colisionador::CalculeFuerza(Cuerpo * Planeta){
-  int i,j;
-  for(i=0;i<N;i++)
-    Planeta[i].BorreFuerza();
-  for(i=0;i<N;i++)
-    for(j=i+1;j<N;j++)
-      CalculeFuerzaEntre(Planeta[i],Planeta[j]);
-}
-void Colisionador::CalculeFuerzaEntre(Cuerpo & Planeta1, Cuerpo &Planeta2){
-  vector3D r21,n,F1; double d21,F;
-  r21 = Planeta2.r - Planeta1.r; d21=r21.norm(); n=r21/d21;
-  F = G*Planeta1.m*Planeta2.m*pow(d21,-2.0);
-  F1=F*n;
-  Planeta1.SumeFuerza(F1); Planeta2.SumeFuerza(F1*(-1));
-}
-//------------------ Funciones globales ------------------------//
+
 int main(){
   Cuerpo Planeta[N];
   Colisionador Newton;
@@ -123,6 +64,5 @@ int main(){
     for(i=0;i<N;i++) Planeta[i].Mueva_v(dt,Coeficiente1);     // 2
     for(i=0;i<N;i++) Planeta[i].Mueva_r(dt, Zeta);            // 1
   }
-  //std::cout<<T<<std::endl;
   return 0;
 }
