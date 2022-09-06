f5d2bc8f781909b52c51b65c0d5143eeb9b2c613
Laura Juliana Caviedes Betancourt <ljcaviedesb@unal.edu.co>
2022-09-05 21:57:39 -0500

    Add troyan planet

--- Taller1/Problema3/Sistema_Sol_Jupiter.cpp
+++ Taller1/Problema3/Sistema_Sol_Jupiter.cpp
@@ -3,7 +3,7 @@
 #include "../vector.h"
 #include <fstream>
 /////////////////////////////////////////////////////////////////
-//Dos Cuerpos Celestes - metodo de integracion PEFRL           //
+// Cuerpos Celestes - metodo de integracion PEFRL              //
 //                                                             //
 // En este programa se simula y anima el movimiento ente dos   //
 // planetas con un observador ajeno al sistema, a este último  //
@@ -11,15 +11,20 @@
 // Los objetos celestes son el Sol y Júpiter a quienes vamos a //
 // asignar las masas y radios como razones entre ellos:        //
 //                                                             //
-// m_sol = 1047 m_jupiter    ,  r_sol = 9.73 r_jupiter         //
-// m_jupiter = 1             ,  r_jupiter = 1                  //
+// m_sol = 1047 m_jupiter     ,  r_sol = 9.73 r_jupiter        //
+// m_jupiter = 1              ,  r_jupiter = 1                 //
 //                                                             //
+// Tambien se agrega un tercer objeto al cual se le denomina   //
+// troyano de Júpiter y va a estar ubicado en el punto de      //
+// Lagrange L4 el cuel esta a pi/3 en adelanto a Júpiter. Su   //
+// masa y radio se escogen:                                    //
+// m_troyano = 0.005 m_jupiter ,  r_troyano = 1_jupiter/10     //
 /////////////////////////////////////////////////////////////////
 const double G=1.0;
-// Tenemos dos planetas
-const int N=2; // Nos permitirá cerar un arreglo de dos planetas
+// Para el análisis de Sol-Júpiter hacemos N=2. Para
+// Sol-Jupiter-Troyano hacemos N=3
+const int N=3;
 
-// constantes globales
 // Coeficientes de PEFRL
 const double Zeta   = 0.1786178958448091e00;
 const double Lambda = -0.2123418310626054e00;
@@ -91,23 +96,26 @@ void Colisionador::CalculeFuerzaEntre(Cuerpo & Planeta1, Cuerpo &Planeta2){
 int main(){
   Cuerpo Planeta[N];
   Colisionador Newton;
-  double m1=1047, m2=1, r=1000;
-  double M=m1+m2, x1=-m2*r/M, x2=m1*r/M;
+  double m1=1047, m2=1, m3=0.005, r=1000;
+  double theta=M_PI/3;
+  double M=m1+m2, x1=-m2*r/M, x2=m1*r/M, x3=x2*cos(theta),y3=x2*sin(theta);
   double omega = sqrt(G*M/pow(r,3.0)), T=2*M_PI/omega, V1=omega*x1, V2=omega*x2;
-  double t,tmax=21*T, dt=0.01;
+  double Vx3=-V2*sin(theta), Vy3=V1*cos(theta);
+  double t, dt=0.01;
+  double tmax=21.0*T;
   double tdibujo, tcuadro=T/100;
   int i;
   std::ofstream outfile;
 
   outfile.open("S-J_original_axis.dat");
 
-  Planeta[0].Inicie(x1,0,0,0,V1,0,m1,97.3);
-  Planeta[1].Inicie(x2,0,0,0,V2,0,m2,10.0);
-  
-  //InicieAnimacion();
+  Planeta[0].Inicie(x1, 0,0,  0, V1,0,m1,97.3); // Sol
+  Planeta[1].Inicie(x2, 0,0,  0, V2,0,m2,10.0); // Júpiter
+  Planeta[2].Inicie(x3,y3,0,Vx3,Vy3,0,m3, 1.0); // Planeta Troyano
   for(t=0,tdibujo=0; t<tmax; t+=dt, tdibujo+=dt){
     outfile<<Planeta[0].Getx()<<" "<<Planeta[0].Gety()<<" "
-	   <<Planeta[1].Getx()<<" "<<Planeta[1].Gety()<<std::endl;
+	     <<Planeta[1].Getx()<<" "<<Planeta[1].Gety()<<" "
+	     <<Planeta[2].Getx()<<" "<<Planeta[2].Gety()<<std::endl;
     // Haga el movimiento y los calculos por PEFRL
     for(i=0;i<N;i++) Planeta[i].Mueva_r(dt, Zeta);            // 1 
     Newton.CalculeFuerza(Planeta);
--- Taller1/Problema3/plot_SJ.cpp
+++ Taller1/Problema3/plot_SJ.cpp
@@ -6,12 +6,13 @@ int main(void){
   //string filename;
   //cout<<"Please enter the name of the .dat file: ";
   //cin>> filename;
-  cout<<"set size square "<<endl;
+  //cout<<"set size square "<<endl;
   cout<<"set multiplot"<<endl;
   cout<<"set grid"<<endl;
   //cout<<"plot \""<<filename<<"\" using 1:2 w l"<<endl;
   cout<<"plot \"S-J_original_axis.dat\" using 3:4 title \"Jupiter\" w l,";
-  cout<<" \"S-J_original_axis.dat\" using 1:2 title \"Sol\" w l"<<endl;
+  cout<<" \"S-J_original_axis.dat\" using 1:2 title \"Sol\" w l,";
+  cout<<" \"S-J_original_axis.dat\" using 5:6 title \"Troyano\" w l"<<endl;
   cout<<"set origin .25,.25"<<endl;
   cout<<"set size square .5,.5"<<endl;
   cout<<"set label \"Sol\""<<endl;
